b2d23bd77393a1a9e5b0281f83679db77727905d
Maksim Eremeev <eremeevmiami94@icloud.com>
2021-10-30 18:28:19 +0300

    the task is done

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,5 +43,11 @@ export default {
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    build: {
+      dir: 'dist'
+    },
+    generate: {
+      dir: 'dist'
+    }
   }
 }
